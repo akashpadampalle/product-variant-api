d4f88bd7a376a59ebe6e357a9eacef34330e3ba2
Akash Padampalle <Akashpadampalle780@gmail.com>
2024-01-13 12:17:20 +0530

    Delete dist directory

--- dist/schemas/variants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const zod_1 = require("zod");
-const variantSchema = zod_1.z.object({
-    name: zod_1.z.string().min(1).max(50),
-    addition_cost: zod_1.z.number().min(1),
-    stock_count: zod_1.z.number().min(1),
-});
-exports.default = { variantSchema };
